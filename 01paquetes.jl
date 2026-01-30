0033da3a70f7be142097e9f0d4e99ed0d5bb5321
Arturo Erdély <arturoerdely@gmail.com>
2026-01-30 17:10:06 -0600

    Update 01paquetes.jl

--- 01paquetes.jl
+++ 01paquetes.jl
@@ -1,23 +1,18 @@
 #=
-
-    Instala algunos paquetes externos necesarios para el curso de 
-    Estadística II mediante un ciclo. También podrían instalarse
-    uno por uno desde el modo paquete en la terminal mediante
-    la instrucción `add` seguida del nombre del paquete.
-
+    Instalar algunos paquetes necesarios para el curso
 =#
 
-using Pkg # cargando el instalador de paquetes externos
+using Pkg # cargando el instalador de paquetes de Julia
 
 begin
     paquete = ["QuadGK", "HCubature", "Optim",
                "SpecialFunctions", "LaTeXStrings", 
                "Distributions", "StatsBase",
-               "Plots", "StatsPlots"
+               "Plots", "StatsPlots", "CSV", "DataFrames"
     ]
     for p in paquete
         println("*** Instalando paquete: ", p)
         Pkg.add(p)
     end
-    println("*** Fin de la lista de paquetes.")
+    println("*** Fin de lista de paquetes.")
 end
